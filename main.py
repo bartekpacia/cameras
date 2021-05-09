24844966b5cd1d5baa7754e9d1333873d1060696
Bartek Pacia <barpac02@gmail.com>
2021-05-10 00:21:35 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+import sys
+import os
+
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
+
+username = sys.argv[1]
+password = sys.argv[2]
+idc = sys.argv[3]
+
+url_2 = f"rtsp://{username}:{password}@192.168.1.100:554/mode=real&idc={idc}&ids=1"
+
+capture = cv2.VideoCapture(url_2, cv2.CAP_FFMPEG)
+
+while(1):
+  ret, frame = capture.read()
+  if ret == False:
+    print("Frame is empty")
+    break
+  else:
+    cv2.imshow("VIDEO", frame)
+    cv2.waitKey(1)
+
+capture.release()
+cv2.destroyAllWindows()
