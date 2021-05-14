0144c27184f8f1455d6bf9936395cdfad7700aa9
Bartek Pacia <barpac02@gmail.com>
2021-05-14 13:08:22 +0200

    use env vars in the Go version

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bartekpacia/cameras
 
 go 1.16
 
-require gocv.io/x/gocv v0.27.0
+require (
+	github.com/joho/godotenv v1.3.0
+	gocv.io/x/gocv v0.27.0
+)
--- main.go
+++ main.go
@@ -4,36 +4,50 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gocv.io/x/gocv"
 )
 
 var (
 	user     string
 	password string
+	address  string
+	port     string
 	idc      string
 )
 
 func init() {
 	log.SetFlags(0)
-	flag.StringVar(&user, "user", "", "")
-	flag.StringVar(&password, "password", "", "")
-	flag.StringVar(&idc, "idc", "", "")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("failed to load environment variables")
+	}
+
+	user = os.Getenv("USER")
+	password = os.Getenv("PASSWORD")
+	address = os.Getenv("ADDRESS")
+	port = os.Getenv("PORT")
+	idc = os.Getenv("IDC")
 }
 
 func main() {
 	flag.Parse()
-	url := fmt.Sprintf("rtsp://%s:%s@192.168.1.100:554/mode=real&idc=%s&ids=1", user, password, idc)
+	url := fmt.Sprintf("rtsp://%s:%s@%s:%s/mode=real&idc=%s&ids=1", user, password, address, port, idc)
 	capture, err := gocv.OpenVideoCapture(url)
 	if err != nil {
 		log.Fatalln("failed to open video capture:", err)
 	}
-	window := gocv.NewWindow("capture")
+	window := gocv.NewWindow("video capture " + idc)
 	img := gocv.NewMat()
 
 	for {
 		capture.Read(&img)
 		window.IMShow(img)
 		window.WaitKey(1)
+
+		fmt.Printf("type: %s, bytes: %d\n", img.Type(), len(img.ToBytes()))
 	}
 }
