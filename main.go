9288bdc3333264f9106b53602089f44f4f6f8e5d
Bartek Pacia <barpac02@gmail.com>
2021-05-14 14:11:02 +0200

    add writing video to file

--- main.go
+++ main.go
@@ -35,19 +35,41 @@ func init() {
 
 func main() {
 	flag.Parse()
+
 	url := fmt.Sprintf("rtsp://%s:%s@%s:%s/mode=real&idc=%d&ids=1", user, password, address, port, idc)
 	capture, err := cv.OpenVideoCapture(url)
 	if err != nil {
 		log.Fatalln("failed to open video capture:", err)
 	}
 	window := cv.NewWindow("video capture " + fmt.Sprint(idc))
+
 	img := cv.NewMat()
+	defer img.Close()
+
+	if ok := capture.Read(&img); !ok {
+		log.Fatalln("failed to read a frame from video capture to matrix")
+	}
+
+	filename := fmt.Sprintf("cam_%d.mkv", idc)
+	videoWriter, err := cv.VideoWriterFile(filename, "X264", 10, img.Cols(), img.Rows(), true)
+	if err != nil {
+		log.Fatalln("failed to create VideoWriter:", err)
+	}
+	defer videoWriter.Close()
 
 	for {
-		capture.Read(&img)
+		if ok := capture.Read(&img); !ok {
+			log.Fatalln("failed to read a frame from video capture to matrix")
+		}
+
 		window.IMShow(img)
 		window.WaitKey(1)
 
-		fmt.Printf("type: %s, bytes: %s\n", img.Type(), lenReadable(len(img.ToBytes()), 2))
+		err = videoWriter.Write(img)
+		if err != nil {
+			log.Fatalln("failed to write a frame to VideoWriter")
+		}
+
+		fmt.Printf("new frame: type: %s, bytes: %s\n", img.Type(), lenReadable(len(img.ToBytes()), 2))
 	}
 }
