0fa076f5a483c9cac2e5cfcaa1fc4f48848455b2
Bartek Pacia <barpac02@gmail.com>
2025-01-29 02:59:06 +0100

    main2: improvements

--- main2.py
+++ main2.py
@@ -9,14 +9,15 @@ import numpy as np
 
 import torch
 
-print(
-    f"Metal Performance Shaders backend available? {torch.backends.mps.is_available()}"
-)
-print(f"cuda available? {torch.cuda.is_available()}")
+backend_mps_available = torch.backends.mps.is_available()
+backend_cuda_available = torch.backends.cudnn.is_available()
 
+print(f"Metal Performance Shaders available? {backend_mps_available}")
+print(f"CUDA available? {backend_cuda_available}")
 
-settings.update({'datasets_dir': Path.home() / '.cache' / 'ultralytics'})
-model = YOLO("yolo11n.pt")
+settings.update({"datasets_dir": str(Path.home() / ".cache")})
+print("set datasets_dir to", settings["datasets_dir"])
+model = YOLO("yolo11n.pt")  # yolov8m.pt, yolo11n.pt
 
 cap = cv2.VideoCapture(sys.argv[1])
 while True:
@@ -24,28 +25,31 @@ while True:
     if not ret:
         break
 
-    results = model(frame, device="mps")
-    result = results[0]
-    bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
-    classes = np.array(result.boxes.cls.cpu(), dtype="int")
-    # print(result)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print(f"{fps=}")
 
-    for cls_idx, bbox in zip(classes, bboxes):
-        (x, y, w, h) = bbox
-        cls_name = model.names[cls_idx]
-        if cls_name == 'tv':
+    results = model.predict(frame, device="mps")
+    print(f'found {len(results)} results')
+    for result in results:
+        if not result.boxes:
             continue
 
-        cv2.putText(
-            img=frame,
-            text=cls_name,
-            org=(x, y - 10),
-            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale=0.5,
-            color=(0, 255, 0),
-            thickness=2,
-        )
-        cv2.rectangle(frame, pt1=(x, y), pt2=(w, h), color=(0, 255, 0), thickness=2)
+        bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
+        classes = np.array(result.boxes.cls.cpu(), dtype="int")
+
+        for cls_idx, bbox in zip(classes, bboxes):
+            (x, y, w, h) = bbox
+            cls_name = model.names[cls_idx]
+            cv2.putText(
+                img=frame,
+                text=cls_name,
+                org=(x, y - 10),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.5,
+                color=(0, 255, 0),
+                thickness=2,
+            )
+            cv2.rectangle(frame, pt1=(x, y), pt2=(w, h), color=(0, 255, 0), thickness=2)
 
     cv2.imshow("frame", frame)
     key = cv2.waitKey(1)
